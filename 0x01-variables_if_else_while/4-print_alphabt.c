7a8a28f96c8983a30c5e3638265a366391e35076
Emmanuel <eadiran07@gmail.com>
2023-02-17 16:49:15 -0800

    alphabet3

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	char c;
 
-	c='a';
+	c = 'a';
 
 	while
 		(c <= 'z') {
-			if ((c != 'q' && c != 'e') &&c <= 'z')
+			if ((c != 'q' && c != 'e') && c <= 'z')
 				putchar(c);
 			c++;
 		}
