5bec142431a051367c10007230ff766f7671fbdc
Emmanuel <eadiran07@gmail.com>
2023-02-22 05:58:15 -0800

    timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - prints multiplication table
+ *
+ */
+
+void times_table(void)
+{
+	int i, n;
+	int calc = n * 1;
+
+	for (n = 0; n < 9 n++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			_putchar(calc + '0');
+			if (i < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+}
