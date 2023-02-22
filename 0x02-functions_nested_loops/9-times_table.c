2e8633de71db2b6fa512e365d7b323d9c02efc16
Emmanuel <eadiran07@gmail.com>
2023-02-22 06:15:30 -0800

    timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,13 @@
 void times_table(void)
 {
 	int i, n;
-	int calc = n * 1;
 
 	for (n = 0; n < 9; n++)
 	{
 		for (i = 0; i <= 9; i++)
 		{
-			_putchar(calc );
+			int calc = n * 1;
+			_putchar(calc);
 			if (i < 9)
 			{
 				_putchar(',');
