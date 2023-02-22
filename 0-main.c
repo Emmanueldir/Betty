6a8f6c5a3d904040178b259829582275049d1d7c
Emmanuel <eadiran07@gmail.com>
2023-02-22 09:31:32 -0800

    multiplemains

---
+++ 0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests for positive or negative integers
+ *
+ * Return: 0 (gives a positive output) 1 (otherwise)
+ */
+
+int main(void)
+{
+	int i = 2;
+
+	positive_or_negative(i);
+	return (0);
+}
