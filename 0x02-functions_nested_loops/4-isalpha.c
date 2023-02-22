dfa71976f98336ccc71e4ceb5c19186f6e48ad22
Emmanuel <eadiran07@gmail.com>
2023-02-21 17:30:05 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabet chacrater
+ *
+ * Result: 1 (success) 0 (otherwise)
+ *
+ * @c: the progm to be checked
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c - 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
