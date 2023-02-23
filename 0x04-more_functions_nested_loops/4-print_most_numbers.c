e897e1364eb64278ee1126d97b20450bf2f7e7aa
Emmanuel <eadiran07@gmail.com>
2023-02-23 11:34:48 -0800

    pmnumbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most_number - prints nubers from 0-9
+ * Description: prints d numbers except 2 & 4
+ * Return: numbers from 0-9
+ */
+
+void print_most_numbers(void)
+
+{
+	int x = 0;
+
+	for (x = 0; x <=9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
