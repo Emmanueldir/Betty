950770825a43283d581d26aca2f6490526a5bd70
Emmanuel <eadiran07@gmail.com>
2023-02-22 19:08:50 -0800

    timetable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,5 +8,31 @@
 
 void print_times_table(int n)
 {
-	
+	int i;
+
+	for (n = 0; n < 13; n++)
+	{
+		for (i = -; i <= 12; i++)
+		{
+			int calc = n * i;
+
+			_putchar(calc + '0');
+
+			if (i < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(calc + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((calc / 10) + '0');
+				_putchar((calc % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}	
 }
