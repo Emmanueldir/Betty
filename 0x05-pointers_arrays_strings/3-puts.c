cf20a47dd311e207131a9e24bacc9798bce93f92
Emmanuel <eadiran07@gmail.com>
2023-02-27 04:38:28 -0800

    pstring

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints a string.
+ * @str: the string to be printed
+ * Return: 0 (void)
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
