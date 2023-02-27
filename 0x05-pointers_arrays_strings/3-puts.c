5d1cf011283ab31e295a15823234e9fb6ad26389
Emmanuel <eadiran07@gmail.com>
2023-02-27 07:34:44 -0800

    pstring

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string
- * @str: The string to printed
- * Return: void
+ * _puts - Prints a string followed by a new line
+ * @str: var for str
  */
 
 void _puts(char *str)
-
 {
 	for (; *str != '\0'; str++)
 	{
-		_putchar(str);
+		_putchar(*str);
 	}
 	_putchar('\n');
 }
