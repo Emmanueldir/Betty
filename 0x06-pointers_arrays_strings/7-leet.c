550323fe52012eb9f441c9cfc5c9a1c6c8fc0ce8
Emmanuel <eadiran07@gmail.com>
2023-03-03 18:18:35 -0800

    encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,29 @@
 #include "main.h"
+
+/**
+ * leet - encodes a string
+ * @s: input
+ * Return: the pointer to dest
+ */
+
+char *leet(char *s)
+{
+	int count = 0, i;
+	int lowletters[] = {97, 101, 111, 116, 108};
+	int upletters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == lowletters[i] || *(s + count) == upletters[i])
+			{
+				*(s + count) = numbers[i];
+				break;
+			}
+		}
+		count++;
+	}
+	return (s);
+}
