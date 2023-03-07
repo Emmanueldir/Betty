5cb514d34dfce9c04442323c540c39aea7a4ed45
Emmanuel <eadiran07@gmail.com>
2023-03-07 07:20:23 -0800

    characterstr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
-/**
- * _strchr - function that locates a character in a string
+/* _strchr - function that locates a character in a string
  * @s: pointer variable to char datatype
  * @: pointer variable to char datatype
  * Return: pointer to the first occurrence of the character c or NULL if none
-is found
  */
 
 char *_strchr(char *s, char c)
@@ -20,8 +18,7 @@ char *_strchr(char *s, char c)
 		}
 		else
 		{
-			return (NULL);
+			return ('\0');
 		}
 	}
-	return (NULL);
 }
