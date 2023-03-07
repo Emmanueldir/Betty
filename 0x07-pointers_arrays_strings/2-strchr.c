4ef634c64a64d4ffb6b53451a90e22d3c6f1b15f
Emmanuel <eadiran07@gmail.com>
2023-03-07 09:53:19 -0800

    characterstr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,15 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return (s[i]);
+			return (&s[i]);
 		}
 	}
+	if (s[i] == c)
+		return (&s[i]);
+
 	return ('\0');
 }
