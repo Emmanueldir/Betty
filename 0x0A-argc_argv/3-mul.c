a2d767aed5130b6597dad26d911fd8b2e4fc9ae6
Emmanuel <eadiran07@gmail.com>
2023-03-14 09:44:42 -0700

    print mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,19 +11,23 @@
 
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result;
-
-	result = num1 * num2;
-
-	printf("%d\n", result);
-	return (0);
+	int i;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (1);
+		return(1);
 	}
+
+	for (i = 1; i < argc; i++)
+	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+		int result = num1 * num2;
+
+		printf("%d\n", result);
+		return (0);
+	}
+	return (0);
 
 }
