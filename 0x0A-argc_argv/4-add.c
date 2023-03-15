7b56fd68739b94a5b357899a4163245d8cc38b52
Emmanuel <eadiran07@gmail.com>
2023-03-15 14:03:04 -0700

    add pnum

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 /**
  * main - add positive numbers
  * @argc: argument count
@@ -10,5 +12,27 @@
 
 int main(int argc, char *argv[])
 {
-	
+	int i, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!(isdigit(atoi(argv[i]))))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+
 }
