abdfec8015ba40e176d19b84c893f79e36096ec8
Emmanuel <eadiran07@gmail.com>
2023-03-21 14:39:18 -0700

    Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * struct dog - sturcture for dog details
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner
+ *
+ * Description: This is a structure that collects dog details
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
