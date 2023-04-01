f0245909bec6b3013ff85feb57396aef4d784e97
Emmanuel <eadiran07@gmail.com>
2023-03-31 17:49:23 -0700

    resolved task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,15 +11,3 @@ void printJ(void)
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
-/**
- * main - main function
- * Return: 0 success
- */
-
-int main(void)
-{
-	printJ();
-
-	return (0);
-}
