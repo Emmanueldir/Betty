a6d3e72ca6695ba38058e1acd4697e8c17f78b20
Emmanuel <eadiran07@gmail.com>
2023-03-31 17:45:40 -0700

    solved task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * printJ - prints a sentence
+ * Returns: nothing
+ */
+
+void printJ(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+/**
+ * main - main function
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	printJ();
+
+	return (0);
+}
