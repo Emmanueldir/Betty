d79f8cb33ea7d61edf9959d3996da8bc73e8b15c
Emmanuel <eadiran07@gmail.com>
2023-04-05 15:48:20 -0700

    resolved task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -16,9 +16,9 @@ void free_listint2(listint_t **head)
 	while (*head != NULL)
 	{
 		temp = (*head);
-		temp->next = (*head)->next;
+		temp = (*head)->next;
 		free(*head);
 	}
 
-	*head = NULL;
+	head = NULL;
 }
